43415d8e9cc338cccbf2bfe3f8222339d36a94d0
Ersan <ersandagdeviren@gmail.com>
2024-04-18 13:35:27 +0300

    term deleted

--- Simetri/order/views.py
+++ Simetri/order/views.py
@@ -53,7 +53,6 @@ def comparison(request):
 
 def customer_list(request):
     customer_list = []
-    term=1
     customer_selected=request.session['customers']
     product_form=ProductSearchForm(request.POST)
 
