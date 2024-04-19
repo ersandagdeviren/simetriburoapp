1d7050a470643bdfac964e3e1c40a9661d3bebd3
Ersan <ersandagdeviren@gmail.com>
2024-04-19 10:26:59 +0300

    deneme

--- Simetri/order/views.py
+++ Simetri/order/views.py
@@ -52,6 +52,10 @@ def comparison(request):
 
 
 def customer_list(request):
+
+    if "customner" not in request.session:
+        request.session["customer"]=[]
+        
     customer_list = []
     customer_selected=request.session['customers']
     product_form=ProductSearchForm(request.POST)
