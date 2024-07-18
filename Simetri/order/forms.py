022a7ca0a03ba1f6e2dc0f7d57c9078ca49edcbb
Ersan <ersandagdeviren@gmail.com>
2024-07-18 23:57:40 +0300

    small changes in template

--- Simetri/order/forms.py
+++ Simetri/order/forms.py
@@ -17,7 +17,7 @@ class ProductForm(forms.Form):
 class ProductSearchForm(forms.Form):
     product_name = forms.CharField(
         max_length=100,
-        label='Search Product',
+        label='Ürün Ara',
         widget=forms.TextInput(attrs={'placeholder': 'Ürün'})
     )
 class ProductaddForm(forms.Form):
