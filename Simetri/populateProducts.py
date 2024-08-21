cb21a5c6bc7857d7b4a78a5d6304ebdad152b98b
ersandagdeviren <ersandagdeviren@gmail.com>
2024-08-21 21:15:04 +0300

    local server

--- Simetri/populateProducts.py
+++ Simetri/populateProducts.py
@@ -12,43 +12,43 @@ from order.models import Product, unit, brand, mainCategory, category, currency
 from django.core.exceptions import ObjectDoesNotExist
 
 # Read Excel File
-#df = pd.read_excel('/Users/ersandagdeviren/Desktop/simetriburoapp/Simetri/order/static/ProductFinal.xls')
-df= pd.read_excel(r'C:\Users\MALIHP\Desktop\simetriburoapp\Simetri\order\static\ProductFinal.xls')
+df = pd.read_excel(r'C:\Users\MALIHP\Desktop\simetriburoapp\Simetri\order\static\ProductFinal.xls')
+
 # Iterate Through Rows
 for index, row in df.iterrows():
     try:
         # Query for unit
         unit_instance = unit.objects.get(unitProduct=row['unit'])
     except unit.DoesNotExist:
-        # Handle the case where the unit does not exist
+        print(f"Unit '{row['unit']}' does not exist for product: {row['codeUyum']}")
         continue
 
     try:
         # Query for brand
         brand_instance = brand.objects.get(brand=row['brand'])
     except brand.DoesNotExist:
-        # Handle the case where the brand does not exist
+        print(f"Brand '{row['brand']}' does not exist for product: {row['codeUyum']}")
         continue
 
     try:
         # Query for main category
         main_category_instance = mainCategory.objects.get(mainCategory=row['mainCategory'])
     except mainCategory.DoesNotExist:
-        # Handle the case where the main category does not exist
+        print(f"Main Category '{row['mainCategory']}' does not exist for product: {row['codeUyum']}")
         continue
 
     try:
         # Query for category
         category_instance = category.objects.get(category=row['category'])
     except category.DoesNotExist:
-        # Handle the case where the category does not exist
+        print(f"Category '{row['category']}' does not exist for product: {row['codeUyum']}")
         continue
 
     try:
         # Query for currency
         currency_instance = currency.objects.get(currency=row['currency'])
     except currency.DoesNotExist:
-        # Handle the case where the currency does not exist
+        print(f"Currency '{row['currency']}' does not exist for product: {row['codeUyum']}")
         continue
 
     # Try to retrieve the product by its unique identifier (e.g., code or codeUyum)
@@ -91,5 +91,8 @@ for index, row in df.iterrows():
             final_product=row['final_product']
         )
 
-    # Save the instance (whether updated or newly created) to the database
-    product_instance.save()
+    try:
+        # Save the instance (whether updated or newly created) to the database
+        product_instance.save()
+    except Exception as e:
+        print(f"Failed to save product '{row['codeUyum']}': {e}")
