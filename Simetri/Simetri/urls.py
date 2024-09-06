4a58726e309cdc650a0d7747c78e7f3f7a1d5af3
Ersan <ersandagdeviren@gmail.com>
2024-09-06 20:57:32 +0300

    back to order

--- Simetri/Simetri/urls.py
+++ Simetri/Simetri/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-from Simetri.users import views
+from users import views
 
 
 
--- Simetri/order/urls.py
+++ Simetri/order/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name="Simetri.order"
+app_name="order"
 urlpatterns = [
     path("", views.main, name="main"),
     path("search/",views.search, name ="search"),
