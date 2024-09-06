38721c9ac67b8136b17e6f6b917ba43748c49171
Ersan <ersandagdeviren@gmail.com>
2024-09-06 22:17:19 +0300

    parent1-1

--- Simetri/Simetri/settings.py
+++ Simetri/Simetri/settings.py
@@ -34,8 +34,8 @@ ALLOWED_HOSTS =os.environ.get("ALLOWED_HOSTS").split(" ")
 # Application definition
 
 INSTALLED_APPS = [
-    "Simetri.order",
-    "Simetri.users",
+    "order",
+    "users",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
