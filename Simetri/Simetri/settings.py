24353c9b0dd4fb09295e209a9106d02e987411ab
Ersan <ersandagdeviren@gmail.com>
2024-09-06 23:34:55 +0300

    pls

--- Simetri/Simetri/settings.py
+++ Simetri/Simetri/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = "Simetri.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "order", "Templates")], 
+        "DIRS": [], 
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
