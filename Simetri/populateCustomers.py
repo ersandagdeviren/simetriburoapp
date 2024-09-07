efd0afd70c6afe81ab3608b97a807c2493eff7cf
Ersan <ersandagdeviren@gmail.com>
2024-09-07 23:23:41 +0300

    customer populate updated

--- Simetri/populateCustomers.py
+++ Simetri/populateCustomers.py
@@ -1,6 +1,7 @@
 import os
 import django
 import pandas as pd
+from django.conf import settings
 
 # Set the DJANGO_SETTINGS_MODULE environment variable
 os.environ['DJANGO_SETTINGS_MODULE'] = 'Simetri.settings'
@@ -10,11 +11,19 @@ django.setup()
 
 from order.models import Customer
 
+# Construct the correct path to the Excel file using STATIC_ROOT in production
+if not settings.DEBUG:
+    file_path = os.path.join(settings.STATIC_ROOT, 'customerData.xls')
+else:
+    # Use the relative path during local development
+    file_path = os.path.join('Simetri/order/static/customerData.xls')
 
-df=pd.read_excel("Simetri/order/static/customerData.xls")
+# Read the Excel file
+df = pd.read_excel(file_path)
 
+# Loop through the rows and create Customer instances
 for index, row in df.iterrows():
-    customer_instance=Customer(
+    customer_instance = Customer(
         customerCode=row['customerCode'],
         companyName=row['companyName'],
         taxOffice=row['taxOffice'],
@@ -29,9 +38,5 @@ for index, row in df.iterrows():
         telephone=row['telephone'],
         customerType=row['customerType'],
         contactPerson=row['contactPerson'],
-
     )
     customer_instance.save()
-
-
-
