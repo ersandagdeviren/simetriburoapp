40c5ca9d761721ca790d7bffb729e2e3e70112e5
Ersan <ersandagdeviren@gmail.com>
2024-09-09 11:23:13 +0300

    populate product updated

--- Simetri/order/views.py
+++ Simetri/order/views.py
@@ -1181,7 +1181,7 @@ def post_invoice(request, invoice_number):
 
     # Configure webdriver options for headless mode
     chrome_options = Options()
-    #chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
