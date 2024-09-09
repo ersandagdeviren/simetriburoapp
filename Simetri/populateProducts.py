1a2b7d86d5f53f71be0b61a798ce9b944a906b49
Ersan <ersandagdeviren@gmail.com>
2024-09-09 12:35:42 +0300

    populate product updated

--- Simetri/populateProducts.py
+++ Simetri/populateProducts.py
@@ -12,7 +12,7 @@ from order.models import Product, unit, brand, mainCategory, category, currency
 from django.core.exceptions import ObjectDoesNotExist
 
 # Read Excel File
-df = pd.read_excel("order/static/productFinal.xls")
+df = pd.read_excel("order/static/ProductFinal.xls")
 
 # Check for duplicate codeUyum values in the Excel file
 duplicated_codes = df[df.duplicated(subset=['codeUyum'], keep=False)]
