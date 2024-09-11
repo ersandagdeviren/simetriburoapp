2dc86e08c2580f4c865087826478a555584570ac
Ersan <ersandagdeviren@gmail.com>
2024-09-11 13:18:33 +0300

    docker final

--- Simetri/order/views.py
+++ Simetri/order/views.py
@@ -1181,11 +1181,11 @@ def post_invoice(request, invoice_number):
 
     # Configure webdriver options for headless mode
     chrome_options = Options()
-    #chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--window-size=1920x1080")
+    #chrome_options.add_argument("--window-size=1920x1080")
     chrome_options.page_load_strategy = 'normal'
 
 
